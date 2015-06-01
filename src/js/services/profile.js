800edc5c222c1b2e54eccfa590b979adcdf02c3f
threehams <threehams@gmail.com>
2015-05-31 17:09:05 -0700

    Rework print view, content, and icons.

--- src/js/services/profile.js
+++ src/js/services/profile.js
@@ -7,6 +7,7 @@ module.exports = {
   email: 'davidaedmondson@gmail.com',
   phone: '818.804.7255',
   link: 'https://github.com/threehams',
+  profile: 'Full-stack developer with 4 years of experience in software development and QA engineering, specializing in automated testing and web development.',
   education: {
     degree: 'Bachelor of Fine Arts - Film and Animation',
     college: 'Rochester Institute of Technology',
@@ -19,23 +20,47 @@ module.exports = {
       endDate: new Date(2015, 3),
       company: 'Sweety High',
       companyLocation: 'Marina Del Rey, CA',
-      summary: 'Full-stack developer working with etc. Quickly promoted from QA Engineer to Software Engineer.',
+      summary: 'Full-stack developer for a social networking site and mobile app API, using Rails, Node.js, jQuery, and Angular. Promoted from QA Engineer to Software Engineer within first year.',
       accomplishments: [
-        'Developed a thing that made things better',
-        'Achieved a thing which resulted in my thing'
+        'Built a content management system with Rails to organize all photos and videos, contests, and curated community photo/video playlists.',
+        'Developed a community moderation system with Rails for approval and rejection of user posted text, photos, and videos.',
+        'Built a Ruby on Rails service that managed all website and mobile user authentication, authorization, account, relationship, and age verification',
+        'Wrote a full suite of load tests, identified, removed, or optimized slow queries, and increased the number of possible requests per second by 400%.',
+        'Wrote the backend API for the video creation, organization, and sharing section of the site.',
+        'Integrated a Node.js API with a third-party moderation system using a reliable message queue.'
       ]
     },
     {
       title: 'QA Engineer',
       startDate: new Date(2010, 8),
-      endDate: new Date(2012, 12),
+      endDate: new Date(2011, 12),
       company: 'Meteor Games',
       companyLocation: 'Beverly Hills, CA',
-      summary: 'Full-stack developer working with etc. Quickly promoted from QA Tester to QA Engineer.',
+      summary: 'Developed pipeline and testing tools and performed automated testing on four Facebook games. Promoted from QA Tester to QA Engineer within first year.',
       accomplishments: [
-        'Developed a thing that made things better',
-        'Achieved a thing which resulted in my thing'
+        'Analyzed content and test workflows for 20 staff to identify sources of inefficiency, prioritize tasks, and develop content management and debugging tools, saving 4K man hours and $150K annually.',
+        'Created a set of scripts to simplify and validate database entry with Python and Selenium, which reduced related bugs by over 90%, and quadrupled department production.',
+        'Automated ticket validation and population processes with Python, reducing weekly process time by 14 hours.',
+        'Built art verification tools to eliminate technical art issues, including a UI used by eight artists and six testers that allowed the QA Department to test art weeks in advance of in-game appearance.',
+        'Taught weekly classes to seven testers on JSON, specific game data formats, keyboard shortcuts, and efficiency.'
       ]
+    }
+  ],
+  projects: [
+    {
+      name: 'Koangulpitestify',
+      summary: 'Full-stack JS boilerplate project, used as a base for other projects. Includes Koa backend and Angular component-based frontend, and is entirely built on promises. Saving a file selectively triggers asset compilation, frontend/backend test runners, server restart, and browser reload as needed.',
+      link: 'https://github.com/threehams/koangulpitestify'
+    },
+    {
+      name: 'Death Whimsy Roadmap',
+      summary: 'Public site to show development progress of a game developed by Squid Blink Games. Written in Angular and Koa, with Flexbox and HTML5 Canvas. Pulls information from the Jira API and receives realtime updates using Jira webhooks.',
+      link: 'https://github.com/threehams/death-whimsy-roadmap'
+    },
+    {
+      name: 'Game Prototypes',
+      summary: 'Four game prototypes using the Phaser game engine, made in 1-2 days each. Done to explore the design complexity of games, and performance difficulties in maintaining 60 frames per second.',
+      link: 'https://github.com/threehams/phaser-game-prototypes'
     }
   ],
   expertise: [
